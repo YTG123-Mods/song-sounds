50c5dff23614e509b5e91b2a60f1bc0a33779573
YTG123 <54103478+YTG1234@users.noreply.github.com>
2021-01-12 08:51:58 +0200

    Switch to async SongManager

--- src/main/kotlin/io/github/ytg1234/songsounds/base/SongManager.kt
+++ src/main/kotlin/io/github/ytg1234/songsounds/base/SongManager.kt
@@ -7,55 +7,81 @@ import io.github.ytg1234.songsounds.MOD_ID
 import io.github.ytg1234.songsounds.base.song.Section
 import io.github.ytg1234.songsounds.base.song.Song
 import io.github.ytg1234.songsounds.logger
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener
+import net.fabricmc.fabric.api.resource.SimpleResourceReloadListener
 import net.minecraft.resource.ResourceManager
 import net.minecraft.util.Identifier
+import net.minecraft.util.profiler.Profiler
 import java.io.IOException
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executor
 import java.util.regex.Pattern
 
-// lmao this is like a black box for me
-object SongManager : SimpleSynchronousResourceReloadListener {
-    private var songs: Map<Identifier, Song> = ImmutableMap.of()
-
-    private val ID = Identifier(MOD_ID, "song_json")
-    private val GSON = GsonBuilder().registerTypeAdapter(Song::class.java, Song.Serializer())
-        .registerTypeAdapter(Section::class.java, Section.Serializer())
-        .create()
+object SongManager : SimpleResourceReloadListener<ImmutableMap<Identifier, Song>> {
+    @Volatile
+    private var songs: ImmutableMap<Identifier, Song> = ImmutableMap.of()
 
     val songIDs: Set<Identifier>
         get() = songs.keys
 
-    override fun getFabricId(): Identifier {
-        return ID
+    private val ID = Identifier(MOD_ID, "song_json")
+    private val GSON = GsonBuilder()
+        .registerTypeAdapter(Song::class.java, Song.Serializer())
+        .registerTypeAdapter(Section::class.java, Section.Serializer())
+        .create()
+
+    override fun getFabricId() = ID
+
+    override fun load(
+        manager: ResourceManager,
+        profiler: Profiler,
+        executor: Executor
+    ): CompletableFuture<ImmutableMap<Identifier, Song>> { // I love async code
+        return CompletableFuture.supplyAsync({
+            profiler.startTick()
+            profiler.push("SongManager")
+
+            val resources = manager.findResources(
+                "songs"
+            ) { it.endsWith(".json") }
+
+            val builder = ImmutableMap.builder<Identifier, Song>()
+
+            for (id in resources) {
+                try {
+                    val jsonObj =
+                        manager.getResource(id).use { JsonParser().parse(it.inputStream.reader()).asJsonObject }
+                    val song = GSON.fromJson(jsonObj, Song::class.java)
+
+                    // Removing "songs/" and ".json" from the path
+                    var newPath = id.path.replaceFirst(Pattern.quote("songs/").toRegex(), "")
+                    val stringBuilder = StringBuilder(newPath)
+                    stringBuilder.replace(newPath.lastIndexOf(".json"), newPath.lastIndexOf(".json") + 5, "")
+                    newPath = stringBuilder.toString()
+                    val newID = Identifier(id.namespace, newPath)
+                    builder.put(newID, song)
+                    logger.info("Successfully loaded $id, new Identifier is $newID")
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            builder.put(Song.EMPTY_ID, Song.EMPTY)
+            val finished = builder.build()
+
+            profiler.pop()
+            finished
+        }, executor)
     }
 
-    override fun apply(manager: ResourceManager) {
-        val resources = manager.findResources(
-            "songs"
-        ) { it.endsWith(".json") }
-
-        val builder = ImmutableMap.builder<Identifier, Song>()
-
-        for (id in resources) {
-            try {
-                val jsonObj = manager.getResource(id).use { JsonParser().parse(it.inputStream.reader()).asJsonObject }
-                val song = GSON.fromJson(jsonObj, Song::class.java)
-
-                // Removing "songs/" and ".json" from the path
-                var newPath = id.path.replaceFirst(Pattern.quote("songs/").toRegex(), "")
-                val stringBuilder = StringBuilder(newPath)
-                stringBuilder.replace(newPath.lastIndexOf(".json"), newPath.lastIndexOf(".json") + 5, "")
-                newPath = stringBuilder.toString()
-                val newID = Identifier(id.namespace, newPath)
-                builder.put(newID, song)
-                logger.info("Successfully loaded $id, new Identifier is $newID")
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-
-        builder.put(Song.EMPTY_ID, Song.EMPTY)
-        songs = builder.build()
+    override fun apply(
+        map: ImmutableMap<Identifier, Song>,
+        manager: ResourceManager,
+        profiler: Profiler,
+        executor: Executor
+    ): CompletableFuture<Void> {
+        return CompletableFuture.runAsync({
+            songs = map
+        }, executor)
     }
 
     operator fun get(id: Identifier) = getSong(id)
